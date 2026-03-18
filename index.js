6c8412a2b6666e298befaae06554c6629497707a
Rafael Sartori Milanez <rafaelsartorimilanez1@gmail.com>
2026-03-18 14:56:44 -0300

    Rotas com multiplos parametros, e mensagem de erro para URLS invalidas

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.put('/users/:id', (req, res) => {
     })
 })
 
+// Enviei um request usando o Postman
 app.delete('/users/:id', (req, res) => {
     const userId = req.params.id
 
@@ -41,6 +42,25 @@ app.delete('/users/:id', (req, res) => {
         message: `User with id: ${userId} is deleted successfuly`
     })
 })
+
+// Rota com multiplos parametros
+
+app.get('/things/:name/:id', (req, res) => {
+    const { name, id } = req.params
+    //id([0-9]{5} o id deve conter exatemente 5 digitos)
+    if (!/^\d{5}$/.test(id)) {
+        return res.status(400).json({ error: 'ID must be exactly 5 digits' });
+    }
+    res.json({
+        id, name
+    })
+})
+
+
+app.get('/*any', (req, res) => {
+    res.send('sorry, this is an invalur URL.')
+})
+
 
 app.listen(port, (req, res) => {
     console.log(`App is running on port ${port}`)
